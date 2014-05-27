5448aacf72dbdd4b4aca846f0765f3bbac25fa83
steele42 <steele42@uw.edu>
2014-05-26 23:01:15 -0700

    fixed a bug

--- CSV_creator_full.py
+++ CSV_creator_full.py
@@ -30,7 +30,7 @@ def main():
 
 	for transcription in os.listdir(TRANSCRIPTION_PATH):
 		vid_id = re.search(r'_([0-9]+)\.txt', transcription).groups()[0]
-		vid_youtube_id = re.search(r'watch\?v=(.+)&?', video_list[ int(vid_id) - 1 ]).groups()[0]
+		vid_youtube_id = re.search(r'watch\?v=(.+)&?', video_list[ int(vid_id) - 1 ]).groups()[0].strip()
 
 		file = open( os.path.join(TRANSCRIPTION_PATH, transcription) ).readlines()
 		text = '"'
